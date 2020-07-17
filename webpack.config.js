84918a2a27b863f40caaac1e0511948e9cdcbc92
Giacomo Corallini <giacomocorallini@Giacomos-MBP.homenet.telecomitalia.it>
2020-07-17 17:15:21 +0200

    aggiunto babel transpiler

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,26 @@
 const path = require("path");
 
 module.exports = {
-    //
+    mode: 'development',
+    entry: {
+        index: './src/app/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './public/js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
 };
