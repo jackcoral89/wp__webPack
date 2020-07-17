c2f7acb3347aa884f66d6518fafecf71627ac1eb
Giacomo Corallini <giacomocorallini@Giacomos-MBP.homenet.telecomitalia.it>
2020-07-17 17:46:16 +0200

    aggiunto browser-sync

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -33,5 +34,12 @@ module.exports = {
                 ],
             },
         ]
-    }
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            proxy: 'http://localhost:3903'
+        })
+    ]
 };
